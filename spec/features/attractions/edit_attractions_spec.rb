4b6ed3fa0e05a3ca2fbe343e0e6b7f9137709190
Derek Maffett <derekgmaffett@gmail.com>
2014-10-06 19:43:21 -0700

    fix authentication calls

--- spec/features/attractions/edit_attractions_spec.rb
+++ spec/features/attractions/edit_attractions_spec.rb
@@ -4,6 +4,6 @@ feature 'editing an attraction' do
   scenario 'a visitor can not access the edit page' do
     visit edit_attraction_path(attractions(:museum))
 
-    page.text.must_include 'You must sign up or sign in before continuing'
+    page.text.must_include 'You need to sign in or sign up before continuing'
   end
 end
