4e2b3374ec6a9fc1908cc6d14422fcebbbe8618b
Derek Maffett <derekgmaffett@gmail.com>
2014-10-06 18:15:34 -0700

    add admins

--- spec/features/auth/sign_in_spec.rb
+++ spec/features/auth/sign_in_spec.rb
@@ -4,12 +4,12 @@ feature 'signing in as admin' do
   scenario 'admin should see flash message confirming sign-in' do
     log_in(:admin)
 
-    page.text.must_include 'Thank you for signing in'
+    page.text.must_include 'Signed in successfully'
   end
 
   scenario 'an improper password prevents log in' do
     log_in_incorrectly
 
-    page.text.must_include 'Invalid password'
+    page.text.must_include 'Invalid email address or password'
   end
 end
