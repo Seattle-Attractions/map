ed33236bd70fc718af81e14b4b9ede5f169cee90
DEIRDRE ALLISON <DEIRDRE9@GMAIL.com>
2014-10-07 16:32:33 -0700

    add some restaurant views and add latitude and longitude to restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007231403) do
+ActiveRecord::Schema.define(version: 20141007233129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 20141007231403) do
     t.string   "Price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
