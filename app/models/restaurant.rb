56cbdef274575112502e76c9da165888fba63ed4
Derek Maffett <derekgmaffett@gmail.com>
2014-12-24 22:47:48 -0800

    ajax restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ActiveRecord::Base
+  geocoded_by :address
+  # auto-fetch coordinates and the condition is for preventing fetching the same address more than once
+  after_validation :geocode, if: :address_changed?
 end
