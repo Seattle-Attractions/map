1bd92d989dcb8070e2e2df7c1d7895de5bc1b9ad
Derek Maffett <derekgmaffett@gmail.com>
2014-12-24 22:47:48 -0800

    ajax restaurants

--- app/models/parking_lot.rb
+++ app/models/parking_lot.rb
@@ -1,2 +1,5 @@
 class ParkingLot < ActiveRecord::Base
+  geocoded_by :address
+  # auto-fetch coordinates and the condition is for preventing fetching the same address more than once
+  after_validation :geocode, if: :address_changed?
 end
