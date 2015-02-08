0bd7fbcf4c5d0a09fa6f781704c9042d46d31fc3
DEIRDRE ALLISON <DEIRDRE9@GMAIL.com>
2015-02-07 23:00:38 -0800

    update unicorn.rb, add new relic

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,8 @@
-worker_processes 3
-timeout 30
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
 preload_app true
 
 before_fork do |server, worker|
-
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
@@ -14,9 +13,8 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
-
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
   defined?(ActiveRecord::Base) and
